f0ed60e5d2bc2fdf35578ce6aa81c072ae1c46de
Bartosz Kostrzewa <bartek@fastmail.net>
2010-11-15 12:31:29 +0100

    added python servertest and launch script, modified server and display

---
+++ frameserver/launchpythons.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+processes=100
+i=0
+
+while [ "$i" -lt "$processes" ]
+do
+	python servertest.py "$i" &
+	pids=( ${pids[@]} $! )
+	echo ${pids["$i"]} 
+	i=$((i+1))
+	sleep 1
+done
+
+#wait for input to exit
+read -n1 -s
+
+#kill our background pythons
+i=0
+while [ "$i" -lt "$processes" ]
+do
+	echo "${pids["$i"]}"
+	kill "${pids["$i"]}"
+	i=$((i+1))
+done	

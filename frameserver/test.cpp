9d3450a5e62aa545676cb4be4f39eba790b2838e
Steve Clement <steve@localhost.lu>
2013-07-16 18:03:18 +0200

    - Amended Makefile so it compiles on Ubuntu 13.04 (tested and frameserver runs and compiles…) - Added more config details to README - Adapted test.cpp but it still doesn't compile

--- frameserver/test.cpp
+++ frameserver/test.cpp
@@ -31,8 +31,8 @@ int main(void)
 
   buffers = new Buffers(NUMBUFS);
 
-  vector<Glib::Threads::Thread::*> readers;
-  vector<Glib::Threads::Thread::*> writers;
+  vector<Glib::Threads::Thread*> readers;
+  vector<Glib::Threads::Thread*> writers;
 
   for(int i = 0; i < NUMTHREADS; i++)
   {
